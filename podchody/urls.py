006dd7d1fc9c263f4c8e07c9d88ddf2305077bae
Kuba <jakub.walawender@rax.com.pl>
2021-04-29 23:35:21 +0200

    test

--- podchody/urls.py
+++ podchody/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from podchody.views import sound
+from podchody.views import sound, test
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', sound),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('test/', test),
 ]
--- podchody/views.py
+++ podchody/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def sound(request):
     return render(request, 'sound.html')
+
+def test(request):
+    return render(request, 'test.html')
+
