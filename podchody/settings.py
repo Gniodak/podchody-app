703bcced8f2befcd824d4d0f24dd40d71f6e35f2
Kuba <jakub.walawender@rax.com.pl>
2021-04-29 23:58:19 +0200

    fix

--- podchody/settings.py
+++ podchody/settings.py
@@ -121,7 +121,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 CKEDITOR_UPLOAD_PATH = 'uploads/'
