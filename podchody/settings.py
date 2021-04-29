a6c8e0c4121308e2f7825bd132a0775165010550
Kuba <jakub.walawender@rax.com.pl>
2021-04-30 01:16:41 +0200

    fix

--- podchody/settings.py
+++ podchody/settings.py
@@ -117,10 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_URL = "/static/"
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
+
+STATIC_ROOT = 'staticfiles'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
