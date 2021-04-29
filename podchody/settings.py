84f8c6f70046e73b5564ec25cd3bcce74dc538db
Kuba <jakub.walawender@rax.com.pl>
2021-04-30 01:09:02 +0200

    fix

--- podchody/settings.py
+++ podchody/settings.py
@@ -119,8 +119,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
